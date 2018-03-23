ed5359cf7a44b858f727f3bb72cfaac6a100812f
BrittanyG <brin.greaner@gmail.com>
2018-03-23 11:36:22 -0500

    updated js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,14 @@
 var getArtistID = function (artistName) {
     //takes argument of artistName and uses it to search Spotify for the ID
     //stores it in a variable
-
+    $.ajax (
+        {
+        method: "GET",
+        url: "https://api.spotify.com/v1/search?q=" + artistName + "&type=artist&limit=100",
+        }).then(function (response) {
+            var spotArtistID = response.items.id
+            console.log (spotArtistID);
+        })
 }
 
     
