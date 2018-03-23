39946c48cebe6b4e5a589b8e965e16f63a09bb5a
BrittanyG <brin.greaner@gmail.com>
2018-03-23 10:06:13 -0500

    adding some psuedo code

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+// need getArtistID (artistName)
+var getArtistID = function (artistName) {
+    //takes argument of artistName and uses it to search Spotify for the ID
+    //stores it in a variable
+
+}
+
+    
+// need var function spotRecommendArtist    
+    //searches for the recommendations using the ID
+    //stores list of recommendations
+
+
+
+
