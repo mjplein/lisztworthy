2ad2f9b30ec9b47ab449064d4621f0112d9eb362
khadija <abdix129@umn.edu>
2018-03-26 22:01:52 -0500

    files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,41 +1,72 @@
 //initialize firebase
-    var config = {
-        apiKey: "AIzaSyDanofEE9ukK5dxUjIAlcVVpkA4CfD_cC0",
-        authDomain: "lisztworthy.firebaseapp.com",
-        databaseURL: "https://lisztworthy.firebaseio.com",
-        projectId: "lisztworthy",
-        storageBucket: "lisztworthy.appspot.com",
-        messagingSenderId: "27323128138"
-    };
-    firebase.initializeApp(config);
+var config = {
+    apiKey: "AIzaSyDanofEE9ukK5dxUjIAlcVVpkA4CfD_cC0",
+    authDomain: "lisztworthy.firebaseapp.com",
+    databaseURL: "https://lisztworthy.firebaseio.com",
+    projectId: "lisztworthy",
+    storageBucket: "lisztworthy.appspot.com",
+    messagingSenderId: "27323128138"
+};
+firebase.initializeApp(config);
 
 
-    //create variable to link firebase
-    var database = firebase.database();
+//create variable to link firebase
+var database = firebase.database();
 
-  $(document).ready(function() {
-  
-// need getArtistID (artistName)
-var getArtistID = function (artistName) {
-    //takes argument of artistName and uses it to search Spotify for the ID
-    //stores it in a variable
-    $.ajax (
-        {
-        method: "GET",
-        url: "https://api.spotify.com/v1/search?q=" + artistName + "&type=artist&limit=100",
-        }).then(function (response) {
-            var spotArtistID = response.items.id
-            console.log (spotArtistID);
+$(document).ready(function() {
+
+
+    // need getArtistID (artistName)
+    function artististID(artistName) {
+        console.log(artistName);
+        //takes argument of artistName and uses it to search Spotify for the ID
+        //stores it in a variable
+        $.ajax({
+            method: "GET",
+            url: "http://ws.audioscrobbler.com/2.0/?method=user.getartisttracks&user=rj&artist=" + artistName + "&api_key=d9f57a8c01d3bfe8ec20acf07e35f0df&format=json",
+        }).then(function(response) {
+            //var spotArtistID = response.items.id
+            //var tracks = response.artisttracks.track;
+
+            //for (var i = 0; i < tracks.length; i++) {
+            // console.log(tracks[i].name);
+            // $("#artistA").append(tracks[i].name);
+            //}
         })
-}
+    }
 
-    
-// need var function spotRecommendArtist    
+    //function songID(artistSong) {
+    // console.log(artistSong);
+
+
+    //create a popover
+    $("#artistB").popover({
+        //clear the popover
+        trigger: 'focus'
+    });
+
+    $("#songA").popover({
+
+        //clear the popover
+        trigger: 'focus'
+    });
+    $("#genreA").popover({
+
+        //clear the popover
+        trigger: 'focus'
+    });
+
+
+    // need var function spotRecommendArtist    
     //searches for the recommendations using the ID
     //stores list of recommendations    
-    
+
     //create click button to add music
     $("#artistB").on("click", function() {
+
+        //popover
+        //$("#artistB").popover();
+
         //var input
         event.preventDefault();
         var artist = $("#artists").val().trim();
@@ -43,12 +74,17 @@ var getArtistID = function (artistName) {
 
     });
     $("#songA").on("click", function() {
-        //var input
+
+
+
+
         event.preventDefault();
         var song = $("#songs").val().trim();
         console.log(song);
+
     });
     $("#genreA").on("click", function() {
+
         //var input
         event.preventDefault();
         var genre = $("#genres").val().trim();
@@ -90,4 +126,3 @@ var getArtistID = function (artistName) {
 
 
 });
-
